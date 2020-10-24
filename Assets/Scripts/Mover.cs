e96d8b4b776287a945dfb8ff0cc4a7f3ee7f3646
Okhunov Odil <odiljon.ahunov.1997@mail.ru>
2020-10-24 19:07:10 +0900

    moving character

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,13 +5,37 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
-
-
-
-    // Update is called once per frame
+    
+   
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+
+        }
+        UpdateAnimator();
     }
+
+        private void MoveToCursor()
+        {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
+    
 }
