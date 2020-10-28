321cc0576ad7fb8777974b8516bad80c71968fbd
Okhunov Odil <odiljon.ahunov.1997@mail.ru>
2020-10-29 02:24:06 +0900

    enemy attack and death

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using RPG.Combat;
 using RPG.Core;
 
 namespace RPG.Movement
 {
 
 
-    public class Mover : MonoBehaviour
+    public class Mover : MonoBehaviour, IAction
     {
         [SerializeField] Transform target;
         NavMeshAgent navMeshAgent;
@@ -27,7 +26,6 @@ namespace RPG.Movement
         public void StartMoveAction(Vector3 destination)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            GetComponent<Fighter>().Cancel();
             MoveTo(destination);
         }
 
@@ -38,10 +36,12 @@ namespace RPG.Movement
             navMeshAgent.isStopped = false;
         }
 
-        public void Stop()
+        public void Cancel()
         {
             navMeshAgent.isStopped = true;
         }
+
+        
 
         private void UpdateAnimator()
         {
